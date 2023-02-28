4387f081eea7d40e132ce548d39dab43809e2047
josephmendez <j.mendez0434@gmail.com>
2023-02-28 12:27:23 -0500

    -Cambios realizados en la interfaz -Actualizacion para el boton de modo oscuro y modo claro, pestaña de proyectos eliminada y puesta en la pagina principal, -Eliminado (comentado) link de cv

--- js/dark-light.js
+++ js/dark-light.js
@@ -1,22 +1,28 @@
 const darkLight = document.querySelector(".dark");
 const dlIcon = document.querySelector(".dark-light-icon");
-const container = document.querySelector(".container-dark");
-const skillsh3 = document.querySelector(".sklls-h3-dark");
+const body = document.querySelector(".body");
+const email = document.getElementById("email");
 
 let dl = true;
 darkLight.addEventListener("click", () => {
     if(dl) {
         darkLight.classList.replace("dark", "light");
         dlIcon.classList.replace("bx-sun", "bx-moon");
-        container.classList.replace("container-dark", "container-light");
+        body.style.backgroundImage = "linear-gradient(to right, #cfcfcf 0%, #fafafa 100%)";
+        body.style.color ="#333";
+        email.style.color="#000";
+        email.style.textShadow = "1px 1px 2px #fff";
         dl = false;
     }
     else {
         darkLight.classList.replace("light", "dark");
         dlIcon.classList.replace("bx-moon", "bx-sun");
-        container.classList.replace("container-light", "container-dark");
+        body.style.backgroundImage = "linear-gradient(to right, #505050 75%, #b3b6b8 150%)";  
+        body.style.color ="#f3f3f3";
+        email.style.color="#fff";
+        email.style.textShadow = "1px 1px 2px #000";
         dl=true;
-    }
+    }   
 })
 
 
