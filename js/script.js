347a1bcec859b8b54e95ebf67a003506ebffa64d
josephmendez <j.mendez0434@gmail.com>
2023-03-30 17:15:11 -0500

    cambio de diseño completo en la pagina, algunos cambios responsive

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ darkLight.addEventListener("click", () => {
     if(dl) {
         darkLight.classList.replace("dark", "light");
         dlIcon.classList.replace("bx-sun", "bx-moon");
-        body.style.backgroundImage = "linear-gradient(to right, #cfcfcf 0%, #fafafa 100%)";
+        body.style.backgroundColor = "#fff";
         body.style.color ="#333";
         email.style.color="#000";
         email.style.textShadow = "1px 1px 2px #fff";
@@ -29,7 +29,7 @@ darkLight.addEventListener("click", () => {
     else {
         darkLight.classList.replace("light", "dark");
         dlIcon.classList.replace("bx-moon", "bx-sun");
-        body.style.backgroundImage = "linear-gradient(to right, #505050 75%, #b3b6b8 150%)";  
+        body.style.backgroundColor = "#1a253c";  
         body.style.color ="#f3f3f3";
         email.style.color="#fff";
         email.style.textShadow = "1px 1px 2px #000";
