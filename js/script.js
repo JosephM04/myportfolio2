c5eb2b4045b6856eee5dcd0381fd56d37294425a
josephmendez <j.mendez0434@gmail.com>
2023-04-06 13:35:23 -0500

    cambios responsive, eliminado el boton y la funcion para cambiar entre modo oscuro y modo claro

--- js/script.js
+++ js/script.js
@@ -10,36 +10,41 @@ window.addEventListener("load", () => {
 // configuración para cambiar entre modo oscuro y modo claro
 const darkLight = document.querySelector(".dark");
 const dlIcon = document.querySelector(".dark-light-icon");
-const body = document.querySelector(".body");
-const email = document.getElementById("email");
-const navA = document.querySelectorAll(".nav-a-dark");
+// const body = document.querySelector(".body");
+// const email = document.getElementById("email");
+// const navA = document.querySelectorAll(".nav-a-dark");
 
-// si dl es true está activado el modo oscuro  y cambiará a modo claro, de lo contrario está en modo claro y cambiará a modo oscuro
-let dl = true;
-// evento al dar click en el boton
-darkLight.addEventListener("click", () => {
-    for (let i = 0; i < navA.length; i++) {
-        navA[i].classList.toggle("nav-a-light");
-    }
-    if (dl) {
-        darkLight.classList.replace("dark", "light");
-        dlIcon.classList.replace("bx-sun", "bx-moon");
-        body.style.backgroundColor = "#fff";
-        body.style.color = "#333";
-        email.style.color = "#000";
-        email.style.textShadow = "1px 1px 2px #fff";
-        dl = false;
-    }
-    else {
-        darkLight.classList.replace("light", "dark");
-        dlIcon.classList.replace("bx-moon", "bx-sun");
-        body.style.backgroundColor = "#1a253c";
-        body.style.color = "#f3f3f3";
-        email.style.color = "#fff";
-        email.style.textShadow = "1px 1px 2px #000";
-        dl = true;
-    }
-})
+// // si dl es true está activado el modo oscuro  y cambiará a modo claro, de lo contrario está en modo claro y cambiará a modo oscuro
+// let dl = true;
+
+// body.style.backgroundColor = "#1a253c";
+// body.style.color = "#f3f3f3";
+
+
+// // evento al dar click en el boton
+// darkLight.addEventListener("click", () => {
+//     for (let i = 0; i < navA.length; i++) {
+//         navA[i].classList.toggle("nav-a-light");
+//     }
+//     if (dl) {
+//         darkLight.classList.replace("dark", "light");
+//         dlIcon.classList.replace("bx-sun", "bx-moon");
+//         body.style.backgroundColor = "#fff";
+//         body.style.color = "#333";
+//         email.style.color = "#000";
+//         email.style.textShadow = "1px 1px 2px #fff";
+//         dl = false;
+//     }
+//     else {
+//         darkLight.classList.replace("light", "dark");
+//         dlIcon.classList.replace("bx-moon", "bx-sun");
+//         body.style.backgroundColor = "#1a253c";
+//         body.style.color = "#f3f3f3";
+//         email.style.color = "#fff";
+//         email.style.textShadow = "1px 1px 2px #000";
+//         dl = true;
+//     }
+// })
 
 // menu de navegación
 
